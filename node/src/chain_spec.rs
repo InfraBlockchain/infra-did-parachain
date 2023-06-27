657b50554f36c3d3bdeb80320c8331faa399d921
Cute_Wisp <sweatpotato13@gmail.com>
2023-06-27 11:07:21 +0900

    refactor: 💡 change chain spec config

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -141,7 +141,7 @@ pub fn development_config() -> ChainSpec {
         None,
         None,
         Extensions {
-            relay_chain: "".into(), // You MUST set this to the correct network!
+            relay_chain: "infrablockspace-local".into(), // You MUST set this to the correct network!
             para_id: INFRA_DID_PARACHAIN_ID,
         },
     )
@@ -219,7 +219,7 @@ pub fn local_testnet_config() -> ChainSpec {
         Some(properties),
         // Extensions
         Extensions {
-            relay_chain: "".into(), // You MUST set this to the correct network!
+            relay_chain: "infrablockspace-local".into(), // You MUST set this to the correct network!
             para_id: INFRA_DID_PARACHAIN_ID,
         },
     )
