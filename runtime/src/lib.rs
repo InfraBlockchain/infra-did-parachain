1c15587ade3a2b329c0eec78870ff8d83e05c1e9
Cute_Wisp <sweatpotato13@gmail.com>
2023-07-07 11:41:59 +0900

    fix: 🐛 fix build error

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,7 +41,10 @@ use sp_api::impl_runtime_apis;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
 use sp_runtime::{
     create_runtime_str, generic, impl_opaque_keys,
-    traits::{AccountIdLookup, BlakeTwo256, Block as BlockT, ConvertInto, IdentifyAccount, Verify},
+    traits::{
+        AccountIdConversion, AccountIdLookup, BlakeTwo256, Block as BlockT, ConvertInto,
+        IdentifyAccount, Verify,
+    },
     transaction_validity::{TransactionSource, TransactionValidity},
     ApplyExtrinsicResult, MultiSignature,
 };
